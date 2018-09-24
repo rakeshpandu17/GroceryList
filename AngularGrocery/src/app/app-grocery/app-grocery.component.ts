00a6101d9f0a82ab270762afe2d2e887a10bef21
Rakesh E Pandu <rakeshpandu17@gmail.com>
2018-09-24 12:41:26 +0800

    Onclick, Enter fn, final commit

--- AngularGrocery/src/app/app-grocery/app-grocery.component.ts
+++ AngularGrocery/src/app/app-grocery/app-grocery.component.ts
@@ -3,27 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-app-grocery',
   templateUrl: './app-grocery.component.html',
-  styleUrls: ['./app-grocery.component.css']
+  styleUrls: ['../../assets/css/bootstrap.min.css'] // added bootstrap file. Use ../ for up one directory & ../../ for up two directory
 })
 
-
-
-
-export class AppGroceryComponent implements OnInit {
-  task: string;
-  tasks = [];
+// removed implement Angular Onint (on initialization) as we dont need to initialise anything while loding the page
+export class AppGroceryComponent {
+  task: string; // to get the text input from textbox
+  tasks = []; // to store the inputs in an array
 
   onClick(){
-    this.tasks.push({name: this.task});
-  this.task = '';
+    this.tasks.push({name: this.task}); // storing the obtained inputs from text box in to an array
+  this.task = ''; // Empty the task variable to enter the next grocery item 
 }
-
-  constructor() { 
-    
-  }
-
-  ngOnInit() {
-  }
-
- 
 }
